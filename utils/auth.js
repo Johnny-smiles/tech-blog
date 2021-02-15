2f6649e72f278064d78a35c91ad81dc6317abb6b
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2021-02-14 19:52:33 -0600

    added handlebars views

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,6 @@
 // adding security to make sure user is logged in before accessing dashboard
 const withAuth = (req, res, next) => {
+  // if not, dispalyed login screen.
     if (!req.session.user_id) {
       res.redirect('/login');
     } else {
